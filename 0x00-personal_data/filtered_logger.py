3d02c4173456428c1078e640e57c236df092ea29
hachimB <boubacarhachim@gmail.com>
2024-05-15 23:24:39 +0100

    filtered_logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Regex-ing
+"""
+import re
+
+
+def filter_datum(fields: list,
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """filter_datum that returns the log message obfuscated"""
+    for field in fields:
+        if field in message:
+            message = re.sub(r'{}=.*?{}'.format(field, separator),
+                             '{}={}{}'.format(field, redaction, separator),
+                             message)
+    return message
