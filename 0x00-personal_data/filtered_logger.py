978447045950d7ceaa9270ec06484cf88210dbdf
hachimB <boubacarhachim@gmail.com>
2024-05-15 23:27:50 +0100

    filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,14 +4,25 @@
 import re
 
 
-def filter_datum(fields: list,
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """filter_datum that returns the log message obfuscated"""
-    for field in fields:
-        if field in message:
-            message = re.sub(r'{}=.*?{}'.format(field, separator),
-                             '{}={}{}'.format(field, redaction, separator),
-                             message)
-    return message
+# def filter_datum(fields: list,
+#                  redaction: str,
+#                  message: str,
+#                  separator: str) -> str:
+#     """filter_datum that returns the log message obfuscated"""
+#     for field in fields:
+#         if field in message:
+#             message = re.sub(r'{}=.*?{}'.format(field, separator),
+#                              '{}={}{}'.format(field, redaction, separator),
+#                              message)
+#     return message
+
+def filter_datum(
+        fields: list,
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    return re.sub(
+        '|'.join(
+            f'{field}=.*?{separator}' for field in fields),
+        lambda match: f'{match.group().split("=")[0]}={redaction}{separator}',
+        message)
