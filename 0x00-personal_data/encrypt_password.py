441163fe9f44d901560bd4f0af5f88962d2a5625
hachimB <boubacarhachim@gmail.com>
2024-05-16 18:50:00 +0100

    hashed_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Encrypting passwords"""
-from bcrypt import gensalt, hashpw # type: ignore
+from bcrypt import gensalt, hashpw
 
 
 def hash_password(password: str) -> bytes:
     """Hashing a password"""
+    if not isinstance(password, str):
+        raise ValueError("Password must be a string")
     byte = password.encode('utf-8')
     salt = gensalt()
     hashed = hashpw(byte, salt)
