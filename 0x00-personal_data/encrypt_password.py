008954d1a749fdb0b62b471f360af5d2f8081c06
hachimB <boubacarhachim@gmail.com>
2024-05-16 20:19:18 +0100

    hashed_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
-"""Encrypting passwords"""
-from bcrypt import gensalt, hashpw, checkpw
+# """Encrypting passwords"""
+# from bcrypt import gensalt, hashpw, checkpw
+
+
+# def hash_password(password: str) -> bytes:
+#     """Hashing a password"""
+#     byte = password.encode('utf-8')
+#     salt = gensalt()
+#     hashed = hashpw(byte, salt)
+#     return hashed
+
+
+# def is_valid(hashed_password: bytes, password: str) -> bool:
+#     """Check valid password"""
+#     return checkpw(password.encode('utf-8'), hashed_password)
+'''encrypt_password module
+'''
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashing a password"""
-    byte = password.encode('utf-8')
-    salt = gensalt()
-    hashed = hashpw(byte, salt)
-    return hashed
+    '''hash_password function
+    '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Check valid password"""
-    # provided_passwd = password.encode('utf-8')
-    return checkpw(password.encode('utf-8'), hashed_password)
+    '''is_valid function
+    '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
