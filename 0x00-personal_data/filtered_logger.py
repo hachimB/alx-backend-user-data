7c4a083247032d81f07e9bbf65d1fad7e29ab01a
hachimB <boubacarhachim@gmail.com>
2024-05-16 07:41:07 +0100

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""Regex-ing"""
+"""Module documentation"""
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str,
-                 separator: str) -> str:
-    """filter_datum that returns the log message obfuscated"""
-    # for field in fields:
-    #     message = (re.sub(r'{}=.*?{}'.format(field, separator), '{}={}{}'
-    #                       .format(field, redaction, separator), message))
-    # return message
+# def filter_datum(fields: list, redaction: str, message: str,
+#                  separator: str) -> str:
+#     """filter_datum that returns the log message obfuscated"""
+#     for field in fields:
+#         message = (re.sub(r'{}=.*?{}'.format(field, separator), '{}={}{}'
+#                           .format(field, redaction, separator), message))
+#     return message
+
+def filter_datum(fields, redaction, message, separator):
+    """Returns the log message obfuscated"""
+    return re.sub(rf"({'|'.join(fields)})=.*?{re.escape(separator)}",
+                  rf"\1={redaction}{separator}",
+                  message)
