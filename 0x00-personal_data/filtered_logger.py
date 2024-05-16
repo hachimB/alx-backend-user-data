15a9d14d55e55752cde5951e0c1b590c671e6ce0
hachimB <boubacarhachim@gmail.com>
2024-05-16 11:14:24 +0100

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -52,10 +52,10 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> MySQLConnection:
     """get_db function"""
-    db = MySQLConnection(
-        host=os.getenv('PERSONAL_DATA_DB_HOST'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
-    )
-    return db
+    # db = MySQLConnection(
+    #     host=os.getenv('PERSONAL_DATA_DB_HOST'),
+    #     user=os.getenv('PERSONAL_DATA_DB_USERNAME'),
+    #     password=os.getenv('PERSONAL_DATA_DB_PASSWORD'),
+    #     database=os.getenv('PERSONAL_DATA_DB_NAME')
+    # )
+    # return db
