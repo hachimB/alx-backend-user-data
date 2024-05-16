a8ea5d95081f6fe46d29395f0a8ecea18a6b5f48
hachimB <boubacarhachim@gmail.com>
2024-05-16 08:35:19 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Module documentation"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +12,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = (re.sub(r'{}=.*?{}'.format(field, separator), '{}={}{}'
                           .format(field, redaction, separator), message))
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """init constructor"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format method"""
+        the_format = super().format(record)
+        return filter_datum(self.fields, RedactingFormatter.REDACTION,
+                            the_format, RedactingFormatter.SEPARATOR)
