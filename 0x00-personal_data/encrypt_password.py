b7e3a67e5f6fb1448b27fcee854911fa8f116c62
hachimB <boubacarhachim@gmail.com>
2024-05-16 18:51:09 +0100

    hashed_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,8 +5,6 @@ from bcrypt import gensalt, hashpw
 
 def hash_password(password: str) -> bytes:
     """Hashing a password"""
-    if not isinstance(password, str):
-        return None
     byte = password.encode('utf-8')
     salt = gensalt()
     hashed = hashpw(byte, salt)
