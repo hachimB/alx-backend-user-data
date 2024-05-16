1a504c060b7a2ed2b03cac20cff466e50f35ef47
hachimB <boubacarhachim@gmail.com>
2024-05-16 18:58:58 +0100

    hashed_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,9 +3,15 @@
 from bcrypt import gensalt, hashpw
 
 
+# def hash_password(password: str) -> bytes:
+#     """Hashing a password"""
+#     byte = password.encode('utf-8')
+#     salt = gensalt()
+#     hashed = hashpw(byte, salt)
+#     return hashed
 def hash_password(password: str) -> bytes:
     """Hashing a password"""
-    byte = password.encode('utf-8')
-    salt = gensalt()
-    hashed = hashpw(byte, salt)
+    byte: bytes = password.encode('utf-8')
+    salt: bytes = gensalt()
+    hashed: bytes = hashpw(byte, salt)
     return hashed
