fb344e28bb0d34326e1eb69a477b8bf0d192fac8
hachimB <boubacarhachim@gmail.com>
2024-05-21 16:37:44 +0100

    extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """Basic auth"""
 from .auth import Auth
+from flask import request
 
 
 class BasicAuth(Auth):
     """BasicAuth class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract_base64_authorization_header"""
+        if authorization_header is None or not isinstance(authorization_header,
+                                                          str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
