71b1ceb7f46e9fb8ef4ed60f778d2f5f5434ec9d
hachimB <boubacarhachim@gmail.com>
2024-05-22 00:22:29 +0100

    Basic - Overload current_user - and BOOM!

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,27 +54,31 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        users = User.search({'email': user_email})
-        if not users:
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
             return None
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
         return None
-    
+
     def current_user(self, request=None):
         """current_user"""
         # Get the authorization header
         auth_header = self.authorization_header(request)
 
         # Extract the Base64 part of the authorization header
-        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
 
         # Decode the Base64 part
-        decoded_base64_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        decoded_base64_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
 
         # Extract the user credentials from the decoded Base64 part
-        user_email, user_pwd = self.extract_user_credentials(decoded_base64_auth_header)
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_base64_auth_header)
 
         # Retrieve the User instance for these credentials
         user = self.user_object_from_credentials(user_email, user_pwd)
