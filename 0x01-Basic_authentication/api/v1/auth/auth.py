c8dc4e1ec459a6ccb98a52e171ca21f21016eb04
hachimB <boubacarhachim@gmail.com>
2024-05-21 03:14:14 +0100

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from typing import List, TypeVar
+from flask import Flask, request
+
+
+class Auth:
+    """Auth class to manage the API authentication"""
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if path is in excluded paths"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Get authorization header"""
+        return None
+    
+    def current_user(self, request=None):
+        """current_user"""
+        return None
