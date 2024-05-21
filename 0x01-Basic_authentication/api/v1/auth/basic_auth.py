129052041ca998c73468acc2504dfb2b87c0c2f5
hachimB <boubacarhachim@gmail.com>
2024-05-21 20:17:41 +0100

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,10 +54,16 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
+        # users = User.search({'email': user_email})
+        # if not users:
+        #     return None
+        # for user in users:
+        #     if user.is_valid_password(user_pwd):
+        #         return user
         users = User.search({'email': user_email})
-        if not users:
+        if not users or len(users) > 1:
             return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+        user = users[0]
+        if user.is_valid_password(user_pwd):
+            return user
         return None
