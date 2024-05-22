33de4c2e6fb974a629e9ed239e6a3e632905b21c
hachimB <boubacarhachim@gmail.com>
2024-05-23 00:59:27 +0100

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -25,8 +25,9 @@ else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if  getenv('AUTH_TYPE') == 'session_auth':
-    SessionAuth = getattr(import_module("api.v1.auth.session_auth"), 'SessionAuth')
+if getenv('AUTH_TYPE') == 'session_auth':
+    SessionAuth = getattr(import_module("api.v1.auth.session_auth"),
+                          'SessionAuth')
     auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
