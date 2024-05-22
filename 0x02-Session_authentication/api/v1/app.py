56b995019181d13b5fd8ceee9b3667406c40d632
hachimB <boubacarhachim@gmail.com>
2024-05-23 00:38:03 +0100

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,7 +58,13 @@ def before_request():
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+    # if auth.current_user(request) is None:
+    #     abort(403)
+    try:
+        request.current_user = auth.current_user(request)
+        if request.current_user is None:
+            abort(403)
+    except Exception:
         abort(403)
 
 
