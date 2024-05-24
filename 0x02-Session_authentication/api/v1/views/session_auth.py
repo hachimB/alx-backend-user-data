88066d1c8f233eb51d572ed2e9f5289a7da7878b
hachimB <boubacarhachim@gmail.com>
2024-05-24 11:14:07 +0100

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,25 +5,30 @@ from api.v1.views.users import User
 import os
 
 
-session_auth_views = Blueprint("session_auth_views", __name__, url_prefix="/api/v1")
+session_auth_views = Blueprint(
+    "session_auth_views",
+    __name__,
+    url_prefix="/api/v1")
 
-@session_auth_views.route('/auth_session/login', methods=['POST'], strict_slashes = False)
+
+@session_auth_views.route('/auth_session/login',
+                          methods=['POST'], strict_slashes=False)
 def login():
     """login route"""
     email = request.form.get('email')
     password = request.form.get('password')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     users = User.search({"email": email})
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
-    
+        return jsonify({"error": "no user found for this email"}), 404
+
     user = users[0]
     if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
-    
+        return jsonify({"error": "wrong password"}), 401
+
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
 
