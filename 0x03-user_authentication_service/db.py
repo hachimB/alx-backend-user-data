cf0e7cf9a27f8f72e46f96cd529dd1eb8c55aa01
hachimB <boubacarhachim@gmail.com>
2024-05-28 16:27:34 +0100

    session_authentication

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import Type
 
 from user import Base
 from user import User
@@ -31,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> Type[User]:
         """add a user"""
         n_user = User(email=email, hashed_password=hashed_password)
         self._session.add(n_user)
