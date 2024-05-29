049c2b7c1a7797888611ca5cc110805051ccbe61
hachimB <boubacarhachim@gmail.com>
2024-05-29 11:53:36 +0100

    valid login

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,7 +53,10 @@ class DB:
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """update a user"""
-        user = self.find_user_by(id=user_id)
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError
         for key, value in kwargs.items():
             if hasattr(user, key):
                 setattr(user, key, value)
