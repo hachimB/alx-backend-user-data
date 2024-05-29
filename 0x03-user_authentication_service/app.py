4ff8490a90eb69e2e50150b0bbaff94a6182cf80
hachimB <boubacarhachim@gmail.com>
2024-05-29 16:01:53 +0100

    login route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def login():
     """Login route"""
     email = request.form.get('email')
     password = request.form.get('password')
-    if AUTH.valid_login(email, password) == False:
+    if AUTH.valid_login(email, password) is False:
         abort(401)
     session_id = AUTH.create_session(email)
     response = make_response({"email": email, "message": "logged in"})
