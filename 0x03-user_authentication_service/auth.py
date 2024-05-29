97a55db8faeb9c8a17f45216a1608b747786aaef
hachimB <boubacarhachim@gmail.com>
2024-05-29 01:19:14 +0100

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """Register a user"""
         user_email = self._db._session.query(
             User).filter_by(email=email).first()
