f052f1f3efd7267175758b292f0734f766d7bc3e
hachimB <boubacarhachim@gmail.com>
2024-05-29 13:09:53 +0100

    valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,7 +47,7 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email: str) -> Optional[str]:
+    def create_session(self, email: str) -> str:
         """create session"""
         try:
             user = self._db.find_user_by(email=email)
