fd4ca5a8dfe37ba06fdbc32bc3f55f90d08466e1
hachimB <boubacarhachim@gmail.com>
2024-05-29 18:51:08 +0100

    destroy_session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,7 +49,7 @@ def logout():
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
-            redirect(url_for('index'))
+            return redirect(url_for('index'))
         else:
             abort(403)
 
