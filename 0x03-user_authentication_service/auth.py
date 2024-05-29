ec3dba2c773772d8b478515b9bdeef4fb0b93646
hachimB <boubacarhachim@gmail.com>
2024-05-29 19:26:52 +0100

    profile

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,7 +85,7 @@ class Auth:
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
-            raise ValueError("User not found")
+            raise ValueError
         # try:
         #     user = self._db.find_user_by(email=email)
         #     if not user:
