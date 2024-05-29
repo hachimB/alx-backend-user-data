b15cfd2770cd97cfdb382d81317dc9ae14fbb0dd
hachimB <boubacarhachim@gmail.com>
2024-05-29 19:26:10 +0100

    profile

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,12 +81,11 @@ class Auth:
         """method to get reset password token"""
         try:
             user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError("User not found")
-
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
         # try:
         #     user = self._db.find_user_by(email=email)
         #     if not user:
