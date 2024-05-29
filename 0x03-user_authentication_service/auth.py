0c90a5b3d897c65fba8b724817789ca9e7690200
hachimB <boubacarhachim@gmail.com>
2024-05-29 10:42:00 +0100

    valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,7 +52,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             user.session_id = session_id
-            self._db.update_user(user.id, user.session_id)
             return session_id
         except NoResultFound:
             return None
