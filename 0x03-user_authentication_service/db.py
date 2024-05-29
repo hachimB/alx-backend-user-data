cc85589b19586462876f7ec8a432c63a1f4411f5
hachimB <boubacarhachim@gmail.com>
2024-05-29 16:51:02 +0100

    destroy_session

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,15 +51,6 @@ class DB:
         except InvalidRequestError:
             raise InvalidRequestError
 
-    # def update_user(self, user_id: int, **kwargs) -> None:
-    #     """update a user"""
-    #     user = self.find_user_by(id=user_id)
-    #     for key, value in kwargs.items():
-    #         if hasattr(user, key):
-    #             setattr(user, key, value)
-    #         else:
-    #             raise ValueError
-    #     self._session.commit()
     def update_user(self, user_id: int, **kwargs) -> None:
         """update a user"""
         try:
