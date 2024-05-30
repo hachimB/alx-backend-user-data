ff9fa2780296329e8c2f88b1d7c487071ecc3501
hachimB <boubacarhachim@gmail.com>
2024-05-30 16:48:49 +0100

    update_password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,7 +77,7 @@ def get_reset_password_token():
         abort(403)
     token = AUTH.get_reset_password_token(email)
     if token:
-        return {"email": email, "reset_token": token}, 200
+        return make_response({"email": email, "reset_token": token}), 200
     abort(403)
 
 
